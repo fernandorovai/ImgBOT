54b561fadeb9ee24163ea0804a793f9850549ab2
Fernando Rodrigues Jr <fernandorovai@hotmail.com>
2017-01-21 00:58:10 -0200

    Download Google Images in batches
    - The Bot can download as many images as Google provides for cetain keyword
    - Images are organized in sequence and in folders according to the keyword
    - Save Thumbnail and original size images

--- bot.py
+++ bot.py
@@ -1,28 +1,91 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+######### THIS ROBOT IS FOR EDUCATIONAL PURPOSES #########
+
+import webdriver
 import time
+import urllib
+import os
 
-imgFolder = '/home/junior/Documents/NN/datasets/FoodDevKit/Images'
-driver = webdriver.Firefox()
-toSearch = "feijao"
-url = "https://www.google.com.br/search?hl=pt-PT&site=imghp&tbm=isch&q=" + toSearch
-driver.get(url)
-# assert "Python" in driver.title
-elem = driver.find_elements_by_class_name("ivg-i")
-imgCount = 0
-driver.implicitly_wait(30)  # seconds
+class Bot:
+    def __init__(self, toSearch):
+        self.imgFolder = '/home/junior/Documents/NN/datasets/FoodDevKit/Images/' + toSearch
+        self.driver = webdriver.Firefox()
+        url = "https://www.google.com.br/search?hl=pt-PT&site=imghp&tbm=isch&q=" + toSearch
+        self.driver.get(url)
 
-for element in elem:
-    binary_data = element.screenshot_as_png
-    imgName = imgFolder + "/" + str(imgCount).zfill(7) + ".png"
-    print(imgName)
-    imgCount += 1
-    fd = open(imgName, 'wb')
-    fd.write(binary_data)
-    fd.close()
-    time.sleep(1)
-    #  elem.clear()
-# elem.send_keys("danilo viado")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-driver.close()
+    # Let's browse the entire page first to load all the images and avoid getting stuck on "more results" button
+    # When the document.body.scrollHeight stop changing is because we reached all the results
+    def fetchAllImages(self):
+        moreButton = self.driver.find_element_by_id("smc")
+        style = moreButton.get_attribute("style")
+        lastHeight = 0
+        currentHeight = self.driver.execute_script("return document.body.scrollHeight")
+        while lastHeight < currentHeight:
+            lastHeight = self.driver.execute_script("return document.body.scrollHeight")
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(3)
+            currentHeight = self.driver.execute_script("return document.body.scrollHeight")
+            style = moreButton.get_attribute("style")
+            # We found the "more results" button, let's be polite and click on it =)
+            if "none" not in style:
+                self.driver.find_element_by_id("smb").click()
+        print("All images are available now")
+        return
+
+    def createFile(self, rawData, imgCount, type):
+        if not os.path.exists(self.imgFolder):
+            os.makedirs(self.imgFolder)
+
+        if not os.path.exists(self.imgFolder + "/" + type):
+            os.makedirs(self.imgFolder + "/" + type)
+
+        if rawData is None:
+            print("Stopped")
+        if rawData is not None:
+            if 'base64' in rawData:
+                dataArray = rawData.split(",")
+                data = dataArray[1].decode('base64')
+            else:
+                data = urllib.urlopen(rawData).read()
+
+        imgName = self.imgFolder + "/" + type + "/" + str(imgCount).zfill(7) + ".jpg"
+        fd = open(imgName, 'wb')
+        fd.write(data)
+        fd.close()
+
+    def saveThumbImages(self):
+        elements = self.driver.find_elements_by_class_name("ivg-i")
+        imgCount = 0
+
+        for element in elements:
+            img = element.find_element_by_tag_name("img")
+            rawData = img.get_attribute("src")
+            print(rawData)
+            self.createFile(rawData, imgCount, 'thumb')
+            imgCount += 1
+        self.driver.close()
+
+    def saveBigImages(self):
+        elements = self.driver.find_elements_by_class_name("ivg-i")
+        imgCount = 0
+        prevImg = None
+        srcFullImg = None
+
+        for element in elements:
+            # Click on the image to open the panel
+            element.click()
+            wrapperElements = self.driver.find_element_by_class_name("irc_mi")
+            srcFullImg = wrapperElements.get_attribute("src")
+            # Hack to avoid saving the same image
+            if srcFullImg is not None and srcFullImg != prevImg:
+                self.createFile(srcFullImg, imgCount, 'fullImg')
+                imgCount += 1
+                prevImg = srcFullImg
+        self.driver.close()
+
+# Create the object and pass what you wanna search as a parameter
+bot = Bot("car")
+bot.fetchAllImages()
+# Save thumbnails only
+bot.saveThumbImages()
+# Save the image from the host, it means the image may have better resolution
+bot.saveBigImages()
